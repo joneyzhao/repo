b69a5a3e156f64f66491396d6b475e49b1f13397
zhaohuanhuan <1297972106@qq.com>
2020-06-15 16:59:25 +0800

    重构主体判断设计逻辑

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,36 +1,23 @@
 package com.thoughtworks;
 
-
 public class FizzBuzz {
     public String say(int i) {
-        boolean isDevidedByThreeAndFiveAndSeven = (i%3==0 && i%5==0 && i%7==0);
-        boolean isDevidedByThreeAndFive = (i%3==0 && i%5==0);
-        boolean isDevidedByThreeAndSeven = (i%3==0 && i%7==0);
-        boolean isDevidedByFiveAndSeven = (i%5==0 && i%7==0);
         boolean isDevidedByThree = i%3==0;
         boolean isDevidedByFive = i%5==0;
         boolean isDevidedBySeven = i%7==0;
-        if(isDevidedByThreeAndFiveAndSeven){
-            return  FizzBuzzWhizzConstants.ThreeOutput + FizzBuzzWhizzConstants.FiveOutput + FizzBuzzWhizzConstants.SevenOutput;
-        }
-        if(isDevidedByThreeAndFive){
-            return FizzBuzzWhizzConstants.ThreeOutput + FizzBuzzWhizzConstants.FiveOutput;
-        }
-        if(isDevidedByThreeAndSeven){
-            return FizzBuzzWhizzConstants.ThreeOutput + FizzBuzzWhizzConstants.SevenOutput;
-        }
-        if(isDevidedByFiveAndSeven){
-            return FizzBuzzWhizzConstants.FiveOutput + FizzBuzzWhizzConstants.SevenOutput;
-        }
+        String FizzBuzzWhizzResult = "";
         if(isDevidedByThree) {
-            return FizzBuzzWhizzConstants.ThreeOutput;
+            FizzBuzzWhizzResult = FizzBuzzWhizzConstants.ThreeOutput;
         }
         if(isDevidedByFive){
-            return FizzBuzzWhizzConstants.FiveOutput;
+            FizzBuzzWhizzResult = FizzBuzzWhizzResult + FizzBuzzWhizzConstants.FiveOutput;
         }
         if(isDevidedBySeven){
-            return FizzBuzzWhizzConstants.SevenOutput;
+            FizzBuzzWhizzResult = FizzBuzzWhizzResult + FizzBuzzWhizzConstants.SevenOutput;
         }
-        return String.valueOf(i);
+        if(FizzBuzzWhizzResult == ""){
+            FizzBuzzWhizzResult =  String.valueOf(i);
+        }
+        return FizzBuzzWhizzResult;
     }
 }
